b51ee524f9ef73fe51889a6abdcd0846413819d5
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2023-12-05 20:06:16 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath, "utf8").trim();
+
+function solution(input) {
+  const starList = [];
+  for (let i = 1; i < parseInt(input) + 1; i += 1) {
+    starList.push("*");
+    console.log(starList.join(""));
+  }
+}
+
+solution(input);
