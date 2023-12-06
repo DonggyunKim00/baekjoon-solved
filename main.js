09580950d2d70b57d97076b63197fe569f7c8e1e
donggyun <donggyunkim12@gmail.com>
2023-12-06 14:20:22 +0900

    백준 초기 세팅

---
+++ main.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath, "utf8").trim();
+
+function solution(input) {}
+
+console.log(solution(input));
