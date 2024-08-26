329adbdb3201bf55824b922ca1f48f2ca2f11b25
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2024-08-26 17:08:44 +0900

    [Bronze II] Title: 숫자의 개수, Time: 88 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -1,38 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath, "utf8").trim();
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs
+  .readFileSync(filePath, 'utf8')
+  .toString()
+  .split('\n')
+  .map(Number);
 
-function solution(input) {
-  const numArr = input.split("\n").map((item) => {
-    return parseInt(item);
-  });
-  const allMultiSplit = numArr
-    .reduce((acc, cur) => {
-      return acc * cur;
-    })
-    .toString()
-    .split("");
-  const countObj = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-  };
+const [A, B, C] = input;
+const digit = Array.from({ length: 10 }).fill(0);
 
-  allMultiSplit.forEach((element) => {
-    countObj[element] += 1;
+(A * B * C)
+  .toString()
+  .split('')
+  .forEach((num) => {
+    digit[Number(num)] += 1;
   });
 
-  return Object.values(countObj).forEach((value) => {
-    console.log(value);
-  });
-}
-
-solution(input);
+console.log(digit.join('\n'));
