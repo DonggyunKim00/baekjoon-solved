ded106093844475e52e31d072f60c66a3afdaa3d
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2024-09-11 13:14:23 +0900

    [Silver IV] Title: 카드2, Time: 208 ms, Memory: 28844 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -1,69 +1,40 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = Number(fs.readFileSync(filePath, "utf8").toString().trim());
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath, 'utf8').toString().trim();
 
-class Node {
-  constructor(data) {
-    this.prev = null;
-    this.data = data;
-    this.next = null;
+class Card {
+  constructor(N) {
+    this.list = Array.from({ length: N }, (_, idx) => idx + 1);
+    this.head = 0;
+    this.tail = N;
+  }
+
+  #h_move() {
+    this.head += 1;
+  }
+
+  #t_move() {
+    this.tail += 1;
+  }
+
+  run() {
+    while (this.head < this.tail - 1) {
+      this.#h_move();
+
+      this.list.push(this.list[this.head]);
+      this.#t_move();
+
+      this.#h_move();
+    }
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  addNodeLast(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
-    }
-    this.length++;
-  }
-
-  deleteHead() {
-    if (this.head === null) {
-      return;
-    }
-    if (this.head.next !== null) {
-      this.head.next.prev = null;
-    }
-    this.head = this.head.next;
-    this.length--;
-  }
-  getHead() {
-    return this.head.data;
-  }
-  getSize() {
-    return this.length;
-  }
-}
-
-const logic = () => {
-  const linkedList = new LinkedList();
-
-  for (let i = 1; i <= input; i++) {
-    linkedList.addNodeLast(i);
-  }
-  while (linkedList.getSize() > 1) {
-    linkedList.deleteHead();
-    const head = linkedList.getHead();
-    linkedList.deleteHead();
-
-    linkedList.addNodeLast(head);
-  }
-
-  console.log(linkedList.getHead());
+const solution = (N) => {
+  const card = new Card(N);
+  card.run();
+  return card.list[card.tail - 1];
 };
 
-logic();
+const answer = solution(Number(input));
+console.log(answer);
