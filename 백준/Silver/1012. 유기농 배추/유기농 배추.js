04d5652853aa0f4b6464ba98cb01b23516e5b0d6
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2025-05-06 17:00:11 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 13668 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,82 +1,58 @@
 const fs = require('fs');
 
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const input = fs
-  .readFileSync(filePath, 'utf8')
-  .toString()
-  .trim()
-  .split('\n')
-  .map((item, idx) => {
-    if (!idx) {
-      return Number(item);
-    } else {
-      return item.split(' ').map(Number);
-    }
-  });
+const input = fs.readFileSync(filePath, 'utf8').toString().trim().split('\n');
 
-let totalCase = input.shift();
-const move = [
-  [-1, 0],
-  [0, -1],
-  [1, 0],
-  [0, 1],
+const [dx, dy] = [
+  [1, -1, 0, 0],
+  [0, 0, 1, -1],
 ];
-let answer = [];
 
-const bfs = ([startX, startY], graph) => {
-  const queue = [[startX, startY]];
+const T = Number(input.shift());
+const graph = input.map((item) => item.split(' ').map(Number));
 
-  while (queue.length) {
-    const [px, py] = queue.shift();
+let idx = 0;
+for (let t = 0; t < T; t++) {
+  const [M, N, K] = graph[idx];
+  const arr = graph.slice(idx + 1, idx + K + 1);
 
-    // 지나간 자리는 0으로 바꾸기 => 안하면 무한루프
-    if (!graph[px][py]) continue;
-    else graph[px][py] = 0;
+  const ground = makeGround(M, N, arr);
+  const visited = Array.from({ length: N }, () => Array(M).fill(0));
 
-    // graph에서 4방향 1인지 확인, 1이면 queue에 삽입
-    for (let i = 0; i < 4; i++) {
-      const xpos = px + move[i][0];
-      const ypos = py + move[i][1];
-
-      if (
-        xpos < 0 ||
-        ypos < 0 ||
-        xpos >= graph.length ||
-        ypos >= graph[0].length
-      )
-        continue;
-      if (graph[xpos][ypos]) queue.push([xpos, ypos]);
+  let count = 0;
+  for (let y = 0; y < N; y++) {
+    for (let x = 0; x < M; x++) {
+      if (!ground[y][x] || visited[y][x]) continue;
+      dfs(x, y, ground, visited);
+      count += 1;
     }
   }
-};
 
-while (totalCase > 0) {
-  if (input[0].length === 3) {
-    let sum = 0;
-    const [M, N, K] = input.shift();
-    const pointArr = input.splice(0, K);
-    let graph = Array.from({ length: M }, () =>
-      Array.from({ length: N }, () => 0)
-    );
+  console.log(count);
 
-    // 배추 넣기
-    pointArr.forEach(([x, y]) => {
-      graph[x][y] = 1;
-    });
-
-    for (let k = 0; k < M; k++) {
-      for (let l = 0; l < N; l++) {
-        if (graph[k][l]) {
-          bfs([k, l], graph);
-          sum += 1;
-        }
-      }
-    }
-
-    answer.push(sum);
-  }
-
-  totalCase -= 1;
+  idx += K + 1;
 }
 
-console.log(answer.join('\n'));
+function makeGround(M, N, arr) {
+  const ground = Array.from({ length: N }, () => Array(M).fill(0));
+
+  for (let i = 0; i < arr.length; i++) {
+    const [x, y] = arr[i];
+    ground[y][x] = 1;
+  }
+
+  return ground;
+}
+
+function dfs(x, y, graph, visited) {
+  visited[y][x] = 1;
+
+  for (let dir = 0; dir < 4; dir++) {
+    const mx = x + dx[dir];
+    const my = y + dy[dir];
+    if (mx < 0 || my < 0 || mx >= graph[0].length || my >= graph.length)
+      continue;
+    if (!graph[my][mx] || visited[my][mx]) continue;
+    dfs(mx, my, graph, visited);
+  }
+}
