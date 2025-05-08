75c660bfe4ed1e53e89406f493e02295b9f104a2
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2025-05-08 16:48:09 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -3,84 +3,66 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath, 'utf8').toString().trim().split('\n');
 
+const N = Number(input.shift());
+const graph = input.map((item) => item.split(''));
+
 const [dx, dy] = [
   [1, -1, 0, 0],
   [0, 0, 1, -1],
 ];
 
-// 적록색약일때 컬러를 바꾸는 함수
-const changeColor = (color) => {
-  switch (color) {
-    case 'R':
-    case 'G':
-      return 'G';
-    default:
-      return color;
-  }
-};
+const dfs = (x, y, visited, isColorBlind) => {
+  visited[x][y] = 1;
 
-const bfs = (memo, graph, isColorBlind, start) => {
-  const [curr_x, curr_y] = start;
-  const queue = [[curr_x, curr_y]];
+  for (let dir = 0; dir < 4; dir++) {
+    const mx = x + dx[dir];
+    const my = y + dy[dir];
 
-  memo[curr_x][curr_y] = isColorBlind
-    ? changeColor(graph[curr_x][curr_y])
-    : graph[curr_x][curr_y];
+    if (mx < 0 || my < 0 || mx >= N || my >= N) continue;
+    if (visited[mx][my]) continue;
 
-  let head = 0;
-  let count = 0;
-  while (queue.length > head) {
-    const [prev_x, prev_y] = queue[head++];
-
-    for (let i = 0; i < 4; i++) {
-      const mx = prev_x + dx[i];
-      const my = prev_y + dy[i];
-
-      if (mx < 0 || mx >= N || my < 0 || my >= N) continue;
-      if (memo[mx][my]) continue;
-
-      if (isColorBlind) {
-        if (memo[prev_x][prev_y] !== changeColor(graph[mx][my])) continue;
-      } else {
-        if (memo[prev_x][prev_y] !== graph[mx][my]) continue;
-      }
-      queue.push([mx, my]);
-      memo[mx][my] = isColorBlind ? changeColor(graph[mx][my]) : graph[mx][my];
+    if (isColorBlind) {
+      const str = graph[x][y] + graph[mx][my];
+      if (str == 'RG' || str === 'GR' || graph[x][y] === graph[mx][my])
+        dfs(mx, my, visited, isColorBlind);
+    } else {
+      if (graph[x][y] === graph[mx][my]) dfs(mx, my, visited, isColorBlind);
     }
   }
 };
 
-const solution = (N, graph) => {
-  let usual = Array.from({ length: N }, () =>
-    Array.from({ length: N }, () => 0)
-  );
-  let unusual = Array.from({ length: N }, () =>
-    Array.from({ length: N }, () => 0)
-  );
+const solution = () => {
+  const g_visited = Array.from({ length: N }, () => Array(N).fill(0));
+  const b_visited = Array.from({ length: N }, () => Array(N).fill(0));
 
-  let usualCount = 0;
-  let unusualCount = 0;
+  const answer = {
+    g_cnt: 0,
+    b_cnt: 0,
+  };
 
-  for (let x = 0; x < N; x++) {
-    for (let y = 0; y < N; y++) {
-      // 일반인 기준으로 BFS 탐색
-      if (!usual[x][y]) {
-        bfs(usual, graph, false, [x, y]);
-        usualCount += 1;
-      }
-
-      // 적록색약 기준으로 BFS 탐색
-      if (!unusual[x][y]) {
-        bfs(unusual, graph, true, [x, y]);
-        unusualCount += 1;
+  // 색맹 X
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (!g_visited[i][j]) {
+        dfs(i, j, g_visited, false);
+        answer.g_cnt += 1;
       }
     }
   }
 
-  return [usualCount, unusualCount];
+  // 색맹 O
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (!b_visited[i][j]) {
+        dfs(i, j, b_visited, true);
+        answer.b_cnt += 1;
+      }
+    }
+  }
+
+  return answer;
 };
 
-const N = Number(input.shift());
-const graph = input.map((item) => item.split(''));
-const answer = solution(N, graph).join(' ');
-console.log(answer);
+const { g_cnt, b_cnt } = solution();
+console.log(g_cnt);
+console.log(b_cnt);
