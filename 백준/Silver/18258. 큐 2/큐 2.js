eb1136481f6cdab73cc49de4faadf0158aa893bb
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2025-08-04 11:19:09 +0900

    [Silver IV] Title: 큐 2, Time: 3024 ms, Memory: 532096 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.js
+++ 백준/Silver/18258. 큐 2/큐 2.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs
   .readFileSync(filePath, 'utf8')
   .toString()
@@ -8,77 +8,43 @@ const input = fs
   .split('\n')
   .map((item) => item.split(' '));
 
-class Queue {
-  constructor() {
-    this.list = [];
-    this.left = 0;
-    this.right = 0;
+input.shift();
+
+const answer = [];
+const queue = [];
+let head = 0;
+let tail = 0;
+
+input.forEach(([command, value]) => {
+  if (command === 'push') {
+    queue[tail] = value;
+    tail++;
   }
 
-  push(value) {
-    this.list[this.right] = value;
-    this.right += 1;
-  }
-
-  pop() {
-    if (this.left >= this.right) return -1;
-
-    const value = this.list[this.left];
-    this.left += 1;
-    return value;
-  }
-
-  size() {
-    return this.right - this.left;
-  }
-
-  empty() {
-    return this.size() ? 0 : 1;
-  }
-
-  front() {
-    if (!this.size()) return -1;
-
-    return this.list[this.left];
-  }
-
-  back() {
-    if (!this.size()) return -1;
-
-    return this.list[this.right - 1];
-  }
-}
-
-const [N, ...operate] = input;
-const solution = (n, operate) => {
-  const queue = new Queue();
-  const answer = [];
-
-  operate.forEach(([method, value]) => {
-    switch (method) {
-      case 'push':
-        queue.push(Number(value));
-        break;
-      case 'pop':
-        answer.push(queue.pop());
-        break;
-      case 'size':
-        answer.push(queue.size());
-        break;
-      case 'empty':
-        answer.push(queue.empty());
-        break;
-      case 'front':
-        answer.push(queue.front());
-        break;
-      case 'back':
-        answer.push(queue.back());
-        break;
+  if (command === 'pop') {
+    if (head === tail) {
+      answer.push(-1);
+    } else {
+      answer.push(queue[head]);
+      head++;
     }
-  });
+  }
 
-  return answer;
-};
+  if (command === 'size') {
+    answer.push(tail - head);
+  }
 
-const answer = solution(Number(N[0]), operate).join('\n');
-console.log(answer);
+  if (command === 'empty') {
+    answer.push(head === tail ? 1 : 0);
+  }
+
+  if (command === 'front') {
+    answer.push(head === tail ? -1 : queue[head]);
+  }
+
+  if (command === 'back') {
+    answer.push(head === tail ? -1 : queue[tail - 1]);
+  }
+});
+
+console.log(answer.join('\n'));
