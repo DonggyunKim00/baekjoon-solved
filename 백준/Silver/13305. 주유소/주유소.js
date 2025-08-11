10e8bcaa8df84a36e768b16718ccba96b93b843a
김동균 <110156792+DonggyunKim00@users.noreply.github.com>
2025-08-11 12:20:42 +0900

    [Silver III] Title: 주유소, Time: 256 ms, Memory: 39544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -4,15 +4,13 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath, 'utf8').toString().trim().split('\n');
 
 const N = Number(input.shift());
-const [roadLens, prices] = input.map((v) => v.split(' ').map(Number));
+const [roadLens, prices] = input.map((v) => v.split(' ').map(BigInt));
 
-for (let i = 1; i < N; i++) {
+let answer = prices[0] * roadLens[0];
+
+for (let i = 1; i < N - 1; i++) {
   if (prices[i] > prices[i - 1]) prices[i] = prices[i - 1];
-}
-
-let answer = 0;
-for (let i = 0; i < N - 1; i++) {
   answer += prices[i] * roadLens[i];
 }
 
-console.log(answer);
+console.log(answer.toString());
